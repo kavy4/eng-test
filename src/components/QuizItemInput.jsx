663981893bfeb00c21bbebba660f178a6ccb2dac
kavy4 <tyty020408@gmail.com>
2023-02-16 20:32:57 +0300

    deploy

--- src/components/QuizItemInput.jsx
+++ src/components/QuizItemInput.jsx
@@ -30,7 +30,7 @@ const QuizItemInput = ({ quiz, options, index }) => {
     return (
         <div className='w-9/12 p-5 rounded shadow border mx-auto my-5'>
             <h2 className='text-xl font-semibold'>{index}. { quiz.title }</h2>
-            <input className='block my-5 py-0.5 px-2 rounded shadow-sm border' type="text" disabled={isFinish} value={value} onChange={event => setValue(event.target.value)} />
+            <input className='block my-5 py-0.5 px-2 rounded shadow-sm border' type="text" disabled={isFinish} value={value} onChange={event => setValue(event.target.value)} onKeyUp={event => event.key == 'Enter' && OnClickNextButton()} />
             <Button disabled={isFinish} onClick={() => OnClickNextButton()}>Далее</Button>
             { resultCard && <div className='px-2 py-1 rounded shadow border mt-5'>{ resultCard }</div> }
         </div>
